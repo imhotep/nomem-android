7cba0f159712675119072f7ee1f9a78c13cce52b
Anis Kadri <anis.kadri@gmail.com>
2010-06-02 18:35:48 -0700

    deleting class files. introduced a progress bar

--- src/org/koalabs/nomem/NoMem.java
+++ src/org/koalabs/nomem/NoMem.java
@@ -6,20 +6,24 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,64 +37,92 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class NoMem extends ListActivity {
 
-	private ArrayList<Note> notes;
-	private String apiKey;
 	protected static final int EDIT_NOTE_REQUEST_CODE = 100;
+	private ArrayList<Note> notes = null;
+	private ArrayAdapter<Note> noteAdapter = null;
+	private String host = null;
+	private String apiKey = null;
+	private Runnable viewNotes;
+	private ProgressDialog progressDialog = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		host = getString(R.string.host);
+		apiKey = getString(R.string.api_key);
+		notes = new ArrayList<Note>();
+		noteAdapter = new ArrayAdapter<Note>(this, R.layout.notes, notes);
+		setListAdapter(noteAdapter);
 		
-		apiKey = getString(R.string.api_key); // for some reason this won't work outside of this method.
-		notes = getNotes(3);
+		viewNotes = new Runnable() {
+			@Override
+			public void run() {
+				getNotes();
+			}
+		};
+		Thread thread = new Thread(null, viewNotes, "MagentoBackground");
+		progressDialog = ProgressDialog.show(NoMem.this, "Please wait...", "Retrieving notes", true);
+		thread.start();
 		
-		ArrayAdapter<Note> adapter = new ArrayAdapter<Note>(this, R.layout.notes, notes);
-		setListAdapter(adapter);
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
-
 		lv.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Log.d("DEBUG", "Note ID: "+((Note)parent.getAdapter().getItem(position)).getId());
-				
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
 				// setting data for sub activity
 				Bundle b = new Bundle();
-				b.putString("title", ((Note)parent.getAdapter().getItem(position)).getTitle());
-				b.putString("body", ((Note)parent.getAdapter().getItem(position)).getBody());
-				b.putInt("id", ((Note)parent.getAdapter().getItem(position)).getId());
-				
+				b.putString("title", ((Note) parent.getAdapter().getItem(
+						position)).getTitle());
+				b.putString("body", ((Note) parent.getAdapter().getItem(
+						position)).getBody());
+				b.putInt("id", ((Note) parent.getAdapter().getItem(position))
+						.getId());
+
 				Intent i = new Intent(view.getContext(), NoteActivity.class);
 				i.putExtras(b);
-				
+
 				startActivityForResult(i, EDIT_NOTE_REQUEST_CODE);
-				Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
-	
+
+	private Runnable returnRes = new Runnable() {
+		@Override
+		public void run() {
+			progressDialog.dismiss();
+			noteAdapter.notifyDataSetChanged();
+		}
+	};
+
+
 	private void addNote(Note note, Integer user_id) {
 		try {
+			progressDialog = ProgressDialog.show(NoMem.this, "Please wait...", "Updating note...", true);
 			HttpClient httpClient = new DefaultHttpClient();
-			URI uri = new URI("http://192.168.32.110:3000/notes/"+note.getId());
-			
+			URI uri = new URI(host+"/notes/"
+					+ note.getId());
+
 			// setting up POST HTTP parameters
-			BasicHttpParams put_params = new BasicHttpParams();
-			put_params.setParameter("user_id", user_id);
-			put_params.setParameter("title", note.getTitle());
-			put_params.setParameter("body", note.getBody());
-			
+			List<NameValuePair> putParams = new ArrayList<NameValuePair>(4);
+			putParams
+					.add(new BasicNameValuePair("user_id", user_id.toString()));
+			putParams.add(new BasicNameValuePair("title", note.getTitle()));
+			putParams.add(new BasicNameValuePair("body", note.getBody()));
+
 			HttpPut request = new HttpPut(uri);
 			request.addHeader("API-Key", apiKey);
 			request.addHeader("Accept", "application/json");
-			request.setParams(put_params);
-			
-			Log.d("DEBUG", put_params.toString());
-			
+			request.setEntity(new UrlEncodedFormEntity(putParams));
+
 			HttpResponse response;
 			response = httpClient.execute(request);
 			int status = response.getStatusLine().getStatusCode();
 
-			if(status == HttpStatus.SC_OK) {
+			if (status == HttpStatus.SC_OK) {
 				Log.d("DEBUG", "SUCCESS! \\o/");
 			}
 		} catch (ClientProtocolException e) {
@@ -103,44 +135,49 @@ public class NoMem extends ListActivity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		runOnUiThread(returnRes);
 	}
 
-	private ArrayList<Note> getNotes(int user_id) {
-		ArrayList<Note> notes = new ArrayList<Note>();
+	private void getNotes() {
+		int user_id = 3;
 		try {
 			HttpClient httpClient = new DefaultHttpClient();
-			URI uri = new URI("http://192.168.32.110:3000/notes?user_id="+user_id);
+			URI uri = new URI(host+"/notes?user_id="
+					+ user_id);
 			HttpGet request = new HttpGet(uri);
 			request.addHeader("Accept", "application/json");
 			request.addHeader("User-Agent", "NoMem/Android");
 			request.addHeader("API-Key", apiKey);
-			Log.d("DEBUG", uri.toString());
 			HttpResponse response;
+
 			response = httpClient.execute(request);
 			int status = response.getStatusLine().getStatusCode();
 
-			if(status == HttpStatus.SC_OK) {
+			if (status == HttpStatus.SC_OK) {
 
-					ByteArrayOutputStream ostream = new ByteArrayOutputStream();
-					response.getEntity().writeTo(ostream);
-					String result = ostream.toString();
-					Log.d("DEBUG", result);
+				ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+				response.getEntity().writeTo(ostream);
+				String result = ostream.toString();
+				Log.d("DEBUG", result);
 
-					JSONObject json = new JSONObject(result);
-					Log.d("DEBUG", "<jsonobject>\n"+json.toString()+"\n</jsonobject>");
-					JSONArray nameArray = json.names();
-					JSONArray valArray = json.toJSONArray(nameArray);
-					for(int i = 0 ; i < valArray.length() ; i++) {
-						Log.d("DEBUG", "<jsonname"+i+">\n"+nameArray.getString(i)+"\n</jsonname"+i+">\n"
-								+"<jsonvalue"+i+">\n"+valArray.getJSONArray(i)+"</jsonvalue"+i+">\n");
-						
-						String title = valArray.getJSONArray(i).getString(0);
-						String body = valArray.getJSONArray(i).getString(1);
-						Integer id = Integer.parseInt(nameArray.getString(i));
-						Log.d("DEBUG", "Note/"+id+" : "+title+"/"+body);
-						notes.add(new Note(id, title, body));
-					}
+				JSONObject json = new JSONObject(result);
+				Log.d("DEBUG", "<jsonobject>\n" + json.toString()
+						+ "\n</jsonobject>");
+				JSONArray nameArray = json.names();
+				JSONArray valArray = json.toJSONArray(nameArray);
+				for (int i = 0; i < valArray.length(); i++) {
+					Log.d("DEBUG", "<jsonname" + i + ">\n"
+							+ nameArray.getString(i) + "\n</jsonname" + i
+							+ ">\n" + "<jsonvalue" + i + ">\n"
+							+ valArray.getJSONArray(i) + "</jsonvalue" + i
+							+ ">\n");
+
+					String title = valArray.getJSONArray(i).getString(0);
+					String body = valArray.getJSONArray(i).getString(1);
+					Integer id = Integer.parseInt(nameArray.getString(i));
+					Log.d("DEBUG", "Note/" + id + " : " + title + "/" + body);
+					notes.add(new Note(id, title, body));
+				}
 
 			}
 		} catch (ClientProtocolException e) {
@@ -149,40 +186,42 @@ public class NoMem extends ListActivity {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (JSONException e) {			
-			// TODO Auto-generated catch block			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return notes;
+		runOnUiThread(returnRes);
 	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		Log.d("DEBUG", "Request code: "+requestCode+"/"+resultCode+"/"+data);
-		if(requestCode == EDIT_NOTE_REQUEST_CODE && resultCode == NoteActivity.SUCCESS_RETURN_CODE) {
+		
+		Log.d("DEBUG", "Request code: " + requestCode + "/" + resultCode + "/"
+				+ data);
+		if (requestCode == EDIT_NOTE_REQUEST_CODE
+				&& resultCode == NoteActivity.SUCCESS_RETURN_CODE) {
 			Bundle b = data.getExtras();
-			String title = b.getString("title");
-			String body = b.getString("body");
-			int id = b.getInt("id");
-			
-			Log.d("DEBUG", "Note/"+id+" : "+title+"/"+body);
-			
+			final String title = b.getString("title");
+			final String body = b.getString("body");
+			final int id = b.getInt("id");
+
+			// Log.d("DEBUG", "Note/"+id+" : "+title+"/"+body);
+
 			// finding the right note (maybe there is a better way to do this)
 			Iterator<Note> it = notes.iterator();
 			Note n = null;
-			
-			while(it.hasNext()) {
+			while (it.hasNext()) {
 				n = it.next();
-				if(n.getId() == id) {
+				if (n.getId() == id) {
 					n.setTitle(title);
 					n.setBody(body);
 					break;
 				}
 			}
-			
 			addNote(n, 3);
 		}
 	}
